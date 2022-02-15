404bbb4ccafb9e0b1fba35b99fd42f1cfb2aa07b
anthony.flores <floresgomezanthony@gmail.com>
2022-02-15 11:37:08 -0400

    fe_ts: cleaning entry/patient types

---
+++ fe_ts/src/types/EntryTypes.ts
@@ -0,0 +1,83 @@
+export interface Diagnosis {
+    id: string;
+    code: string;
+    name: string;
+    latin: string;
+}
+
+export enum EntryTypes {
+    HospitalEntry = "Hospital",
+    OccupationalHealthEntry = 'OccupationalHealth',
+    HealthCheckEntry = 'HealthCheck'
+}
+
+export interface Entry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnoses: Array<Diagnosis>;
+    type: EntryTypes;
+}
+
+export interface NewEntry extends Omit<Entry, 'id' | 'diagnoses'> {
+    diagnosisIDs: Array<Diagnosis['code']>;
+}
+
+export interface FormEntry extends NewEntry {
+    //healthCheck
+    healthCheckRating: HealthCheckRating;
+
+    //hospital
+    //discharge: HospitalDischarge;
+
+    //occupational
+    employerName: string;
+    sickLeave: SickLeave;
+}
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+}
+
+export interface HealthCheckEntry extends Entry {
+    healthCheckRating: HealthCheckRating;
+}
+
+export interface NewHealthCheckEntry extends NewEntry {
+    healthCheckRating: HealthCheckRating;
+};
+
+interface HospitalDischarge {
+    date: string;
+    criteria: string;
+}
+
+export interface HospitalEntry extends Entry {
+    discharge?: HospitalDischarge;
+}
+
+export interface NewHospitalEntry extends NewEntry {}
+
+interface SickLeave {
+    startDate: string;
+    endDate: string;
+}
+
+export interface OccupationalHealthEntry extends Entry {
+    type: EntryTypes;
+    employerName: string;
+    sickLeave?: SickLeave;
+}
+
+export interface NewOccupationalHealthEntry extends Omit<OccupationalHealthEntry, 'id' | 'diagnoses' | 'sickLeave'> {
+    diagnosisIDs: Array<Diagnosis['code']>;
+}
+
+export type NewEntries =
+    | NewHospitalEntry
+    | NewOccupationalHealthEntry
+    | NewHealthCheckEntry;
---
+++ fe_ts/src/types/PatientTypes.ts
@@ -0,0 +1,27 @@
+import { Entry } from './EntryTypes';
+
+export enum Gender {
+  Male = "Male",
+  Female = "Female",
+  Other = "Other"
+}
+
+export interface Patient {
+  id: string;
+  name: string;
+  occupation: string;
+  gender: Gender;
+  ssn?: string;
+  dateOfBirth?: string;
+  entries?: Entry[];
+}
+
+export interface NewPatient extends Omit<Patient, 'id' | 'entries'>{};
+
+export interface GetPatientsResponse {
+  Patients: Patient[];
+}
+
+export interface GetPatientResponse {
+  Patient: Patient;
+}
