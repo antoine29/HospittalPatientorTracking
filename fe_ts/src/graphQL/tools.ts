734e5d3ad66b6177785bd2337e8f835f37bf512a
anthony.flores <floresgomezanthony@gmail.com>
2022-02-18 11:20:26 -0400

    fe_ts: fixing tools

--- fe_ts/src/graphQL/tools.ts
+++
@@ -1,6 +0,0 @@
-import { DocumentNode, parse } from 'graphql';
-import { print } from 'graphql/language/printer'
-
-const printGql = (gql: DocumentNode) => {
-    console.log("GQL:", print(gql))
-}
--- fe_ts/src/tools.ts
+++ fe_ts/src/tools.ts
@@ -1,6 +1,13 @@
+import { DocumentNode } from 'graphql';
+import { print } from 'graphql/language/printer';
+
 export const PrintFormatedJson = (_object: object | undefined): string => {
     // ToDo: print _object name instead
     const formattedJson = JSON.stringify(_object, null, 1);
     console.log("_object:", formattedJson);
     return formattedJson;
+}
+
+export const PrintGql = (gql: DocumentNode) => {
+    console.log("GQL:", print(gql))
 }
