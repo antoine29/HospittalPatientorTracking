18c33ec41758964cb25beb9bda5c4a450914b4d7
anthony.flores <floresgomezanthony@gmail.com>
2022-02-21 16:27:32 -0400

    Updated: 2022-02-21 16:27:32

---
+++ fe/gh-page-deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Building latest ui production build folder
+git checkout gh-page
+REACT_APP_DEPLOYMENT_SERVER='gh-pages' \
+REACT_APP_BACKEND_URL='http://localhost:4000/' \
+npm run build
+
+# Coping build folder to docs
+rm -rf ../docs
+cp -r ./build ../docs
+
+git add .
+git commit -m "Updated: `date +'%Y-%m-%d %H:%M:%S'`"
+git push origin
