30f717a69b2ba0fd372ee6c9e4cca3537e8c5a39
anthony.flores <floresgomezanthony@gmail.com>
2022-02-21 16:14:53 -0400

    fe: adding support fro be env url

--- fe/src/graphQL/apolloClient.ts
+++ fe/src/graphQL/apolloClient.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { getBackendUrl } from "../tools";
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
+  //uri: 'http://localhost:4000/',
+  uri: getBackendUrl(),
   cache: new InMemoryCache()
 });
-
--- fe/src/tools.ts
+++ fe/src/tools.ts
@@ -11,3 +11,16 @@ export const PrintFormatedJson = (_object: object | undefined): string => {
 export const PrintGql = (gql: DocumentNode) => {
     console.log("GQL:", print(gql))
 }
+
+export const getBackendUrl = (): string => {
+    const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:4000/'
+    console.log(`Using Backend url: ${backendUrl}`)
+    return backendUrl
+  }
+  
+export const getRouterType = (): string => {
+  const deploymentServer = process.env.REACT_APP_DEPLOYMENT_SERVER
+  const router = deploymentServer === 'gh-pages' ? 'hash' : 'browser'
+  console.log(`Using '${router}' router for ${deploymentServer} deployment server:`)
+  return router; 
+}
